742f71cc8dc0e23221c33584686a52a1b8a68098
Raskolnikov <jerry.tann@opteyes.cn>
2021-04-02 14:05:53 +0800

    feat: implement action creators

---
+++ my-app/src/todo/redux/actions.js
@@ -0,0 +1,19 @@
+let nextTodoId = 0;
+
+export const addToDo = content => ({
+  type: "ADD_TODO",
+  payload: {
+    content,
+    id: ++nextTodoId
+  }
+});
+
+export const toggleTodo = id => ({
+  type: "TOGGLE_TODO",
+  payload: { id }
+});
+
+export const setFilter = filterName => ({
+    type: "SET_FILTER",
+    payload: { filterName }
+})
